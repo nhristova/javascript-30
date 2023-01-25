1a04d376f3ef9ca89f50b22faf75c78fb2db1383
Nadezhda Hristova <epohster@gmail.com>
2023-01-25 19:32:30 +0200

    Inner pages responsive styles, extract home button

---
+++ js/home-button.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+const header = `  <header>
+<a href="../">
+  <img src="../images/noun-home-5451905.svg" title="Main" alt="Created by Kota Tim from Noun Project">
+</a>
+</header>`;
+
+body.insertAdjacentHTML('afterbegin', header);
